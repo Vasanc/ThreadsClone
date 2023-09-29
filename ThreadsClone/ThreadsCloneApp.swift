c695ed5533545aba4c68c37954194294a25e7187
Vasan <vasanc2003@gmail.com>
2023-09-29 18:07:30 +0530

    Initial Commit

---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Vasan on 29/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
